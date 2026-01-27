de925739660ec192db57b9cde39c6b400b8a739f
AndrewLutsky <andrew.lutsky19@gmail.com>
2026-01-27 01:26:02 -0500

    wip

---
+++ sckde.py
@@ -0,0 +1,74 @@
+import scanpy as sc
+from scipy.stats import gaussian_kde
+from anndata import AnnData
+import pandas as pd
+import seaborn as sb
+import numpy as np
+
+def _sckde_single(adata: AnnData, key:str) -> gaussian_kde:
+    """
+    
+    Parameters
+    ----------
+    Adata : AnnData
+        
+        
+    
+    """
+    # TODO
+    return None
+
+def _sckde_multi(adata:AnnData, keys:list[str]):#-> gaussian_kde:
+    """ Function to generate gaussian kde from cells by gene expression matrix,
+    and a list of relevant genes to query the anndata """
+    
+    # Check for keys in the adata
+    for key in keys:
+        if key not in adata.var.index:
+            raise KeysNotFound(key)
+   
+    adata = adata[:, keys].copy()
+
+    # Check for umap key in obsm
+    if 'X_umap' not in adata.obsm:
+        raise KeysNotFound('X_umap') 
+    # Grab the first two UMAP axes
+    um = adata.obsm['X_umap'][:, 0:2]
+
+    kdes = []
+    for key in keys:
+        norm_const = np.sum(adata[:, key].X)
+        if norm_const == 0.:
+            norm_const = 1
+        weights = np.array((adata[:, key].X / norm_const).toarray()).T.reshape(-1)
+        kdes.append(gaussian_kde(um.T, weights =weights))
+   
+    # Grab the UMAP coordinates 
+    um = adata.obsm['X_umap'][:, 0:2].T
+
+    # Instantiate a density dictionary mapping key to a density
+    # distribution. 
+    density_map = {}
+    
+    # Calculate the density for each key
+    for kde, key in zip(kdes, keys):
+        density_map[key] = kde(um)
+        print(um)
+        print(np.sum(kde(um)))
+
+    # Now let us find the joint distribution over all the keys.
+    return np.prod(list(density_map.values())) 
+
+    if len(keys) == 0:
+        "Given keys is empty, utilizing umap-coordinate density!"
+        return gaussian_kde(um.T)
+
+
+
+
+
+# Error Class
+class KeysNotFound(Exception):
+    def __init__(self, key:str):
+        super().__init__(f"{key} not found in the anndata!")
+
